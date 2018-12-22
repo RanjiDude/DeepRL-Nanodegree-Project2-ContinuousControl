c3de671ed8a9e9b809f5b84737bc1d973b1f18e6
Ranjith <ranjitation@gmail.com>
2018-12-21 22:03:33 -0600

    Update ContinuousControl_Test.py

--- ContinuousControl_Test.py
+++ ContinuousControl_Test.py
@@ -20,8 +20,10 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # mini-batch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate
+ACTOR_LR = 10e-4        # Actor learning rate
+CRITIC_LR = 10e-3       # Critic learning rate
 UPDATE_EVERY = 4        # how often to update the network
+
 
 # reset
 env_info = env.reset(train_mode=True)[brain_name]
@@ -33,8 +35,81 @@ print('Number of agents:', len(env_info.agents))
 action_size = brain.vector_action_space_size
 print('Number of actions:', action_size)
 
+# sample a random action
+
 # examine the state space
 state = env_info.vector_observations[0]
 print('States look like:', state)
 state_size = len(state)
 print('States have length:', state_size)
+
+# Defining the actor and critic models
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1./np.sqrt(fan_in)
+    return (-lim, lim)
+
+
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units = 400, fc2_units = 300):
+        """Initialize parameters and build model.
+
+        Params:
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer"""
+
+        super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state):
+        """Build an actor (policy) network that maps states -> actions"""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return F.tanh(self.fc3(x))
+
+
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, seed, fcs1_units = 400, fc2_units = 300):
+        """Initialize parameters and build model.
+        Params:
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each state
+            seed (int): Random seed
+            fcs1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer"""
+
+        super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units + action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state, action):
+        """Build a critic (value) network that maps (state, action) paris -> Q-values."""
+        xs = F.relu(self.fcs1(state))
+        x = torch.cat((xs, action), dim = 1)
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
