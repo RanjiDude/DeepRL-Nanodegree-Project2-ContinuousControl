4b5da2eb79f9c284c572e55eaca61115ed6e5a7e
Ranjith <ranjitation@gmail.com>
2018-12-21 22:50:20 -0600

    Update ContinuousControl_Test.py

--- ContinuousControl_Test.py
+++ ContinuousControl_Test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gym
+import copy
 import random
 from collections import deque, namedtuple
 import matplotlib.pyplot as plt
@@ -17,12 +18,15 @@ brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # mini-batch size
+BATCH_SIZE = 128         # mini-batch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-ACTOR_LR = 10e-4        # Actor learning rate
-CRITIC_LR = 10e-3       # Critic learning rate
+ACTOR_LR = 1e-4         # Actor learning rate
+CRITIC_LR = 1e-3        # Critic learning rate
+WEIGHT_DECAY = 0        # L2 weight decay
 UPDATE_EVERY = 4        # how often to update the network
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 # reset
@@ -113,3 +117,103 @@ class Critic(nn.Module):
         x = F.relu(self.fc2(x))
         return self.fc3(x)
 
+
+# Defining the DDPG agent
+class Agent():
+    """Interacts with the environment and learns from it."""
+
+    def __init__(self, state_size, action_size, random_seed):
+        """Initialize an Agen object.
+
+        Params:
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            random_seed (int): random seed"""
+
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random_seed
+
+        # Actor Network (w/ Target Network)
+        self.actor_local = Actor(state_size, action_size, random_seed).to(device)
+        self.actor_target = Actor(state_size, action_size, random_seed).to(device)
+        self.actor_optimzer = optim.Adam(self.actor_local.parameters(), lr = ACTOR_LR)
+
+        # Critic Network (w/ Target Network)
+        self.critic_local = Critic(state_size, action_size, random_seed).to(device)
+        self.critic_target = Critic(state_size, action_size, random_seed).to(device)
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr = CRITIC_LR, weight_decay = WEIGHT_DECAY)
+
+        # Noise process
+        self.noise = OUNoise(action_size, random_seed)
+
+        #Replay Memory
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+
+    def step(self, state, action, reward, next_state, done):
+        """Save experience in replay memory"""
+        # Save experience / reward
+        self.memory.add(state, action, reward, next_state, done)
+
+        # Learn if enough samples are available in memory
+        if len(self.memory) > BATCH_SIZE:
+            experiences = self.memory.sample()
+            self.learn(experiences, GAMMA)
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, seed, mu = 0., theta = 0.15, sigma = 0.2):
+        """Initialize parameters and noise process."""
+        self.mu = mu*np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (=noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu-x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        self.state = x + dx
+        return self.state
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+        Params:
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch"""
+
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size) #internal memory (deque)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.seed = random.seed(seed)
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+    def sample(self):
+        """Randomly sample a batch of experience from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+
+        return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
